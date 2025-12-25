b40c7c66665d625e834d59fe3105990363ff8525
zoefrnd <yoshuafernando91@gmail.com>
2025-12-25 17:09:28 +0700

    fix vercel standalone error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  reactStrictMode: true,
+
+  // Aman untuk build di Vercel
   typescript: {
     ignoreBuildErrors: true,
   },
-  reactStrictMode: false,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
